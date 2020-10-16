3bae17e103c24794527d44e5ce10fe06bf343fb5
Alexandre Madeira <33880764+MadeiraAlexandre@users.noreply.github.com>
2020-10-16 02:20:00 -0300

    Update ApodClient.cs
    Fixed FetchApodListAsync and FetchApodAsync method

--- src/Celestial/Services/ApodClient.cs
+++ src/Celestial/Services/ApodClient.cs
@@ -17,21 +17,22 @@ namespace Celestial.Services
         {
             try
             {
-                var _client = new HttpClient
+                using (var _client = new HttpClient
                 {
                     BaseAddress = new Uri("https://api.nasa.gov/planetary/apod")
-                };
-                _client.DefaultRequestHeaders.Accept.Clear();
-                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var _startDate = $"{startDate.Year.ToString(CultureInfo.InvariantCulture)}-{startDate.Month.ToString("00", CultureInfo.InvariantCulture)}-{startDate.Day.ToString("00", CultureInfo.InvariantCulture)}";
-                var _endDate = $"{endDate.Year.ToString(CultureInfo.InvariantCulture)}-{endDate.Month.ToString("00", CultureInfo.InvariantCulture)}-{endDate.Day.ToString("00", CultureInfo.InvariantCulture)}";
-                var response = _client.GetAsync(new Uri($"?api_key={Credential.CredentialKey}&start_date={_startDate}&end_date={_endDate}")).Result;
-                var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                var content = JArray.Parse(responseContent);
-                var results = JsonConvert.DeserializeObject<List<Apod>>(content.ToString());
-                results.RemoveAll(x => x.MediaType == "video");
-                _client.Dispose();
-                return results;
+                })
+                {
+                    _client.DefaultRequestHeaders.Accept.Clear();
+                    _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var _startDate = $"{startDate.Year.ToString(CultureInfo.InvariantCulture)}-{startDate.Month.ToString("00", CultureInfo.InvariantCulture)}-{startDate.Day.ToString("00", CultureInfo.InvariantCulture)}";
+                    var _endDate = $"{endDate.Year.ToString(CultureInfo.InvariantCulture)}-{endDate.Month.ToString("00", CultureInfo.InvariantCulture)}-{endDate.Day.ToString("00", CultureInfo.InvariantCulture)}";
+                    var response = _client.GetAsync($"?api_key={Credential.CredentialKey}&start_date={_startDate}&end_date={_endDate}").Result;
+                    var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    var content = JArray.Parse(responseContent);
+                    var results = JsonConvert.DeserializeObject<List<Apod>>(content.ToString());
+                    results.RemoveAll(x => x.MediaType == "video");
+                    return results;
+                }
             }
             catch (Exception)
             {
@@ -42,17 +43,18 @@ namespace Celestial.Services
 
         public static async Task<Apod> FetchApodAsync(DateTimeOffset date)
         {
-            var _client = new HttpClient
+            using (var _client = new HttpClient
             {
                 BaseAddress = new Uri("https://api.nasa.gov/planetary/apod")
-            };
-            _client.DefaultRequestHeaders.Accept.Clear();
-            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var response = _client.GetAsync(new Uri($"?date={$"{date.Year.ToString(CultureInfo.InvariantCulture)}-{date.Month.ToString("00", CultureInfo.InvariantCulture)}-{date.Day.ToString("00", CultureInfo.InvariantCulture)}"}&api_key={Credential.CredentialKey}")).Result;
-            var content = JObject.Parse(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
-            var result = JsonConvert.DeserializeObject<Apod>(content.ToString());
-            _client.Dispose();
-            return result;
+            })
+            {
+                _client.DefaultRequestHeaders.Accept.Clear();
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = _client.GetAsync($"?date={$"{date.Year.ToString(CultureInfo.InvariantCulture)}-{date.Month.ToString("00", CultureInfo.InvariantCulture)}-{date.Day.ToString("00", CultureInfo.InvariantCulture)}"}&api_key={Credential.CredentialKey}").Result;
+                var content = JObject.Parse(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+                var result = JsonConvert.DeserializeObject<Apod>(content.ToString());
+                return result;
+            }
         }
 
     }
